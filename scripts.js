4594a375202ab1ee8451de872806477494a109ca
ECeles <efraim.jr30@gmail.com>
2024-10-14 15:26:42 -0300

    seta prev funcionando e configurado

--- scripts.js
+++ scripts.js
@@ -18,5 +18,9 @@ nextButton.onclick = () => {
 };
 
 prevButton.onclick = () => {
-  console.log("botao prev");
+  let itemOld = container.querySelector(".list .item.active");
+  itemOld.classList.remove("active");
+
+  active = active - 1 < firstPosition ? lastPosition : active - 1;
+  items[active].classList.add("active");
 };
