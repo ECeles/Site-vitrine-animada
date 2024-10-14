1c75b1fa26d11f4413ba63c1d14ecb23f21127b0
ECeles <efraim.jr30@gmail.com>
2024-10-14 15:23:59 -0300

    seta next funcionando e configurado

--- scripts.js
+++ scripts.js
@@ -12,6 +12,9 @@ let lastPosition = items.length - 1;
 nextButton.onclick = () => {
   let itemOld = container.querySelector(".list .item.active");
   itemOld.classList.remove("active");
+
+  active = active + 1 > lastPosition ? 0 : active + 1;
+  items[active].classList.add("active");
 };
 
 prevButton.onclick = () => {
