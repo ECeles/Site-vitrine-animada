13f442dd90f8477782615552fccee67a64b90908
ECeles <efraim.jr30@gmail.com>
2024-10-14 16:31:31 -0300

    verificacao de erro dots e numbers

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ let nextButton = document.getElementById("next");
 let container = document.querySelector(".container");
 let items = container.querySelectorAll(".list .item");
 let indicator = document.querySelector(".indicators");
-let dots = indicator.querySelectorAll("ul il");
+let dots = indicator.querySelectorAll("ul li");
 
 let active = 0;
 let firstPosition = 0;
@@ -15,6 +15,12 @@ nextButton.onclick = () => {
 
   active = active + 1 > lastPosition ? 0 : active + 1;
   items[active].classList.add("active");
+
+  let dotsOld = indicator.querySelector("ul li.active");
+  dotsOld.classList.remove("active");
+  dots[active].classList.add("active");
+
+  indicator.querySelector(".number").innerHTML = "0" + (active + 1);
 };
 
 prevButton.onclick = () => {
