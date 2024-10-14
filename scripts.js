1f4db709517753ceaa9ef39526c0c3cd6d6f0287
ECeles <efraim.jr30@gmail.com>
2024-10-14 15:20:42 -0300

    seta next funcionando

--- scripts.js
+++ scripts.js
@@ -1,14 +1,19 @@
 let prevButton = document.getElementById("prev");
 let nextButton = document.getElementById("next");
-let container = document.querySelector("container");
-let items = container.querySelectorAll(".list . item");
+let container = document.querySelector(".container");
+let items = container.querySelectorAll(".list .item");
 let indicator = document.querySelector(".indicators");
 let dots = indicator.querySelectorAll("ul il");
 
-prevButton.onclick = () => {
-  console.log("botao prev");
-};
+let active = 0;
+let firstPosition = 0;
+let lastPosition = items.length - 1;
 
 nextButton.onclick = () => {
-  console.log("botao next");
+  let itemOld = container.querySelector(".list .item.active");
+  itemOld.classList.remove("active");
+};
+
+prevButton.onclick = () => {
+  console.log("botao prev");
 };
